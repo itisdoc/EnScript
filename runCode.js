08fb5fbaf575189a2f55711c833734cc0e2db379
FireyJS <57120755+TriSn0w@users.noreply.github.com>
2022-05-09 16:02:34 -0400

    Update runCode.js

--- runCode.js
+++ runCode.js
@@ -1,15 +1,17 @@
 let newLines = []
-    var args = process.argv[2]
-if (!args.includes('.en')) throw new Error('you must provide a .en file')
+    var args = process.argv[2] // gets file name from ClI command
+if (!args.includes('.en')) throw new Error('you must provide a .en file') // if file name does not have a extension of ".en", throw a error
 const lineByLine = require('n-readlines');
-const liner = new lineByLine(args);
+const liner = new lineByLine(args); // setup line reader
  
 let line;
 let lineNumber = 0;
  
-while (line = liner.next()) {
-  let lineString = line.toString('ascii')
-  if (lineString.includes('//') || lineString == "") return
+while (line = liner.next()) { // while there is another line
+  let lineString = line.toString('ascii') // make line buffer into ascii string
+  if (lineString.includes('//') || lineString == "") { // if 
+      // dont do nothing
+  } else {
    if (!lineString.includes(';')) {
    if (!lineString.includes('{') || !lineString.includes('}')) {
      throw new Error('(line ' + (lineNumber + 1) + ') You need to add a ";" at the end of every line!')
@@ -24,6 +26,7 @@ while (line = liner.next()) {
       }
      lineNumber++;
    }
+  }
   }
 
 eval(newLines.join(" "))
